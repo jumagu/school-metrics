93ae72a8b323b843d69ce84742f4e7fa398c2a0c
jumagu <jmgg.gutierrez@gmail.com>
2024-08-27 14:54:27 -0500

    feat: Add TeacherSubjectsPage

--- client/src/modules/teacher/index.tsx
+++ client/src/modules/teacher/index.tsx
@@ -2,11 +2,6 @@
 export { default as TeacherLayout } from "./layout/TeacherLayout";
 
 // Pages
-export { TeacherClass } from "./pages/TeacherClass";
-export { TeacherCalendar } from "./pages/TeacherCalendar";
-export { TeacherClassChosen } from "./pages/TeacherClassChosen";
-export { TeacherClassStudents } from "./pages/TeacherClassStudents";
-export { TeacherReport } from "./pages/TeacherReport";
-
 export { default as TeacherChatPage } from "./pages/TeacherChatPage";
 export { default as TeacherContactsPage } from "./pages/TeacherContactsPage";
+export { default as TeacherSubjectsPage } from "./pages/TeacherSubjectsPage";

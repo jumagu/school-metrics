b3d4be4834b428322465605a163919f2bb4b6765
jumagu <jmgg.gutierrez@gmail.com>
2024-09-27 01:01:01 -0500

    refactor: Update Maintenance component to center align text

--- client/src/components/misc/Maintenance.tsx
+++ client/src/components/misc/Maintenance.tsx
@@ -13,10 +13,10 @@ export const Maintenance = () => {
       gap={1}
     >
       <WrenchIcon style={{ width: "80px", height: "80px", color: "#fffffe" }} />
-      <Typography variant="h3" color="white">
+      <Typography variant="h3" color="white" textAlign="center">
         This feature is under maintenance
       </Typography>
-      <Typography variant="body1" color="white">
+      <Typography variant="body1" color="white" textAlign="center">
         sorry for any inconvenience caused. We are working to return the service
         as soon as possible.
       </Typography>

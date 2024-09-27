2274fbf3d43ad2e2494f125c58c9407604603ae3
jumagu <jmgg.gutierrez@gmail.com>
2024-09-27 00:57:23 -0500

    refactor: Update admin and teacher pages to use Maintenance component

--- client/src/modules/teacher/pages/TeacherSubjectsPage.tsx
+++ client/src/modules/teacher/pages/TeacherSubjectsPage.tsx
@@ -1,25 +1,5 @@
-import { Box, Typography } from "@mui/material";
-
-import { WrenchIcon } from "../../../components";
+import { Maintenance } from "../../../components";
 
 export default function TeacherSubjectsPage() {
-  return (
-    <Box
-      height="100%"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      gap={1}
-    >
-      <WrenchIcon style={{ width: "80px", height: "80px", color: "#fffffe" }} />
-      <Typography variant="h3" color="white">
-        This module is under maintenance
-      </Typography>
-      <Typography variant="body1" color="white">
-        sorry for any inconvenience caused. We are working to return the service
-        as soon as possible.
-      </Typography>
-    </Box>
-  );
+  return <Maintenance />;
 }
